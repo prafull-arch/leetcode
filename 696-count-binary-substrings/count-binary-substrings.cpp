e016542d13060aad8422ac4b321650dde7170daa
prafull-arch <vvm12855@gmail.com>
2026-02-20 00:54:04 +0530

    Time: 8 ms (19.95%) | Memory: 12.9 MB (96.95%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.cpp
+++ 696-count-binary-substrings/count-binary-substrings.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
     int countBinarySubstrings(string s) {
-        int result =0;
+        int result = 0;
         int i = 0;
-        while(i<s.length()){
+        while (i < s.length()) {
             int num0 = 0;
             int num1 = 0;
-            if(s[i] == '0'){
-                while(s[i] != '1' && i<s.length()){
+            if (s[i] == '0') {
+                while (s[i] != '1' && i < s.length()) {
                     num0++;
                     i++;
-
                 }
-                int j =i;
-                while(s[j] != '0' && j<s.length()){
-                    num1+= 1;
+                int j = i;
+                while (s[j] != '0' && j < s.length()) {
+                    num1 += 1;
                     j++;
                 }
-                result += min(num0,num1);
+                result += min(num0, num1);
 
-            }
-            else{
-                while(s[i] != '0' && i<s.length()){
+            } else {
+                while (s[i] != '0' && i < s.length()) {
                     num1++;
                     i++;
-
                 }
-                int j =i;
-                while(s[j] != '1' && j<s.length()){
-                    num0+= 1;
+                int j = i;
+                while (s[j] != '1' && j < s.length()) {
+                    num0 += 1;
                     j++;
                 }
-                result += min(num0,num1);
-                
+                result += min(num0, num1);
             }
-            
         }
         return result;
     }
