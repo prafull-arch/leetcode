201c2ddd1989ad251069cb91adfc4fbb0322586d
prafull-arch <vvm12855@gmail.com>
2026-02-26 19:56:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (53.06%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.cpp
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPerfectSquare(int num) {
         if(num<2) return true;
-        for(long long i=1;i<=num/2;i++){
+        for(long long i=1;i*i<=num;i++){
             if(i*i==num) return true;
         }
         return false;
