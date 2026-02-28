efd619cfc8e796a0f7c430b6c45b0b3a5aa109b1
prafull-arch <vvm12855@gmail.com>
2026-02-28 21:19:39 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (80.05%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int Left=0;
-        int Right = s.size() - 1;
-        while(Left<Right){
-            swap(s[Left],s[Right]);
-            Left++;
-            Right--;
-        }
+        reverse(s.begin(),s.end());
+        
     }
 };
